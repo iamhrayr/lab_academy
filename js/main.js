d610c3be9c29b7159975e5d54cda8c45afe139ef
Hrayr <lincerta@gmail.com>
2017-05-17 17:24:44 +0400

    intro animation

--- js/main.js
+++ js/main.js
@@ -1,30 +1,29 @@
-// $(function(){
-
-// 	// init controller
-// 	var controller = new ScrollMagic.Controller();
-
-// 	// create a scene
-// 	var scene = new ScrollMagic.Scene({
-// 		duration: 100,	// the scne should last for a scroll distance of 100px
-// 		offset: 50		// start this scene after scrolling for 50px
-// 	})
-// 	scene.setPin(".lab-svg") // pins the element for the the scene's duration
-// 		.addTo(controller); // assign the scene to the controller
-
-// });
-
-
-
-
-
 $(function() {
 
-  // var controller = new ScrollMagic.Controller();
+    // smooth wheel scroll
+    var $window = $(window);
+    var scrollTime = .5;
+    var scrollDistance = 170;
+
+    $window.on("mousewheel DOMMouseScroll", function(event){
+
+        event.preventDefault(); 
+
+        var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+
+        TweenMax.to($window, scrollTime, {
+            scrollTo : { y: finalScroll, autoKill:true },
+                ease: Power1.easeOut,
+                overwrite: 5                            
+            });
+    });
+
+
+
+    // ScrollMagic inits
     var controller = new ScrollMagic.Controller();
-
-    
-
-
 
     // intro scene
     var tl = new TimelineMax();
@@ -43,17 +42,49 @@ $(function() {
 	.addIndicators()
 	.addTo(controller);
 
+
+    var tl = new TimelineMax();
+    tl.to('#intro-logo', 1, {autoAlpha: .8, y: '-700'})
+        .to('.lab-svg-wrapper', 1.5, {y: -470}, '0.5');
+
 	var scene = new ScrollMagic.Scene({
 		// triggerElement: '#intro', 
 		offset: 800,
 		triggerHook: 'onLeave',
-		duration: 800,
-
+		duration: 1000
 	})
 	.setPin("#intro")
-    // .setTween(tl)
+    .setTween(tl)
 	.addIndicators()
 	.addTo(controller);
+
+
+
+    var tl = new TimelineMax();
+    tl.to('#intro', 1, {y: '-100%'});
+
+    var scene = new ScrollMagic.Scene({
+        // triggerElement: '#intro', 
+        offset: 1800,
+        triggerHook: 'onLeave',
+        duration: 0
+    })
+    .setPin(".lab-svg-wrapper")
+    .setTween(tl)
+    .addIndicators()
+    .addTo(controller);
+
+
+    var scene = new ScrollMagic.Scene({
+        // triggerElement: '#intro', 
+        offset: 1800,
+        triggerHook: 'onLeave',
+        duration: 0
+    })
+    .setPin(".lab-svg-wrapper")
+    // .setTween(tl)
+    .addIndicators()
+    .addTo(controller);
 
 
 	// svg & mouse scene
